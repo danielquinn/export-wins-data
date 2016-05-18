2e052b2ba17a35025db74a116e9dd2f082bcd9b2
Daniel Quinn <code@danielquinn.org>
2016-05-18 14:28:05 +0100

    Added debug logger

--- alice/authenticators.py
+++ alice/authenticators.py
@@ -1,3 +1,5 @@
+import logging
+
 from hashlib import sha256
 
 from django.conf import settings
@@ -7,30 +9,41 @@ from rest_framework.permissions import IsAuthenticated
 
 class AlicePermission(IsAuthenticated):
 
+    def __init__(self):
+        IsAuthenticated.__init__(self)
+        self.logger = logging.getLogger(__name__)
+
     def has_permission(self, request, view):
-        print(request.method, request.get_full_path(), end=" ")
+
+        self.logger.debug(
+            "{} to {}".format(request.method, request.get_full_path()))
+
         if view.action == "schema":
             if self._test_signature(request):
-                print("OK: schema & signature")
+                self.logger.debug("OK: schema & signature")
                 return True
 
         if not IsAuthenticated.has_permission(self, request, view):
-            print("Not authenticated: {}".format(request.META.get("Authorization")))
+            self.logger.debug("Not authenticated: {}".format(
+                request.META.get("Authorization")))
             return False
 
         if self._test_signature(request):
-            print("OK: signature")
+            self.logger.debug("OK: signature")
             return True
-        print("Bad signature")
+
+        self.logger.debug("Bad signature")
+
         return False
 
-    @staticmethod
-    def _test_signature(request):
+    def _test_signature(self, request):
 
         offered = request.META.get("HTTP_X_SIGNATURE")
         salt = bytes(settings.UI_SECRET, "utf-8")
         path = bytes(request.get_full_path(), "utf-8")
         body = request.body
         generated = sha256(path + body + salt).hexdigest()
-        print(generated, "vs.", offered)
+
+        self.logger.debug("{} vs. {}".format(generated, offered))
+
         return generated == offered
