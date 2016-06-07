fc58d871e954429783200cf6112cbf8bc629cc27
Daniel Quinn <code@danielquinn.org>
2016-06-07 16:41:42 +0100

    A better, easier to understand explanation

--- alice/authentication.py
+++ alice/authentication.py
@@ -3,19 +3,27 @@ from rest_framework.authentication import SessionAuthentication
 
 class NoCSRFSessionAuthentication(SessionAuthentication):
     """
-    This is only here because we're guaranteeing the communications between the
-    UI server and the data server to be legit on account of the request signing
-    in Alice.
+    Django REST Framework ships with three authentication systems:
 
-    We're abusing DRF's SessionAuthentication here, which is typically used to
-    auth local requests on the server (for javascript and the BrowseableAPI).
-    Instead, we're spoofing the login session id via alice.rabbit on the ui,
-    and don't want to worry about csrf protection on the data server.  That's
-    why we'r effectively ignoring it for this case.
+    * Basic Auth requires that each request include the username & password. As
+      this would require the UI to retain this information, that's not
+      happening.
+    * Token Auth is what we were using at first, but it's effectively a bearer
+      token system, with static tokens that don't even change after logout.
+    * Session Auth exists so that DRF can be used as part of a website to
+      handle ajax requests etc.  It's also used by the Browseable API.
 
-    If you ever decide to open the data server, you're going to have to modify
-    the UI server to implement Oauth to do things properly.  Have fun with
-    that ;-)
+    Given these three options, the only palatable choice is Session Auth,
+    however because of its typical use case, *it enforces CSRF* which is a pain
+    in the ass for our architecture (the UI is handling CSRF issues, so the
+    data server doesn't have to).
+
+    The fix therefore was to use our own authentication that extends
+    SessionAuthentication and simply skips the CSRF enforcement.  The only
+    other reasonable alternative that I could see is to basically implement
+    Oauth2 on both the UI and the data server -- not something I'm keen on for
+    such a small project.  Once this is rolled into data-hub, the auth
+    component will be thrown out anyway.
     """
 
     def enforce_csrf(self, request):
