f9ee0767627be944eea3f5c5d1dab06d5d83ecbf
Adam Malinowski <adam.malinowski@digital.bis.gov.uk>
2016-07-15 18:24:52 +0100

    multiple ids info and enable changing customer emails & resend

--- wins/management/commands/generate_customer_emailses.py
+++ wins/management/commands/generate_customer_emailses.py
@@ -15,19 +15,55 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--ids", type=str)
-        parser.add_argument("--send", action="store_true",)
+        parser.add_argument("--change_ids", type=str)
+
+    def thing(self, given_win_ids):
+        wins = Win.objects.filter(id__in=given_win_ids)
+        found_win_ids = set(str(win.id).replace('-', '') for win in wins)
+        missing_win_ids = set(given_win_ids) - found_win_ids
+        assert not missing_win_ids, "missing win ids: %s" % missing_win_ids
+        return wins
 
     def handle(self, *args, **options):
         """ Quick hack to generate txt of customer emails and corresponding
         officer emails.
         """
 
-        given_win_ids = [w.replace('-', '') for w in options['ids'].split(',')]
+        if options['ids']:
+            given_win_ids = [w.replace('-', '')
+                             for w in options['ids'].split(',')]
+            wins = self.thing(given_win_ids)
+            change = False
+        elif options['change_ids']:
+            change = True
+            idemails = options['change_ids']
+            id_emails = [idemail.split(':')
+                         for idemail in idemails.split(',') if idemail]
+            id_to_email = {
+                wid.replace('-', ''): email
+                for wid, email in id_emails
+            }
+            wins = self.thing(list(id_to_email.keys()))
 
-        wins = Win.objects.filter(id__in=given_win_ids)
-        found_win_ids = set(str(win.id).replace('-', '') for win in wins)
-        missing_win_ids = set(given_win_ids) - found_win_ids
-        assert not missing_win_ids, "missing win ids: %s" % missing_win_ids
+            for win in wins:
+                win_id = str(win.id)
+                new_customer_email = id_to_email[win_id.replace('-', '')]
+                if win.customer_email_address != new_customer_email:
+                    print(
+                        'win', win_id,
+                        'changing email from ',
+                        win.customer_email_address,
+                        'to',
+                        new_customer_email,
+                        'new email sent'
+                    )
+                    win.customer_email_address = new_customer_email
+                    win.save()
+                else:
+                    print('win', win_id, 'customer email unchanged, email re-sent')
+
+        else:
+            assert False, 'no valid flag given'
 
         for win in wins:
 
@@ -47,17 +83,18 @@ class Command(BaseCommand):
             )
             customer_notification.save()
 
-            officer_email_dict = generate_officer_email(win)
-            send_mail(
-                officer_email_dict['subject'],
-                officer_email_dict['body'],
-                settings.SENDING_ADDRESS,
-                officer_email_dict['to'],
-            )
-            for officer_email in officer_email_dict['to']:
-                officer_notification = Notification(
-                    type=Notification.TYPE_OFFICER,
-                    win=win,
-                    recipient=officer_email,
+            if not change:
+                officer_email_dict = generate_officer_email(win)
+                send_mail(
+                    officer_email_dict['subject'],
+                    officer_email_dict['body'],
+                    settings.SENDING_ADDRESS,
+                    officer_email_dict['to'],
                 )
-                officer_notification.save()
+                for officer_email in officer_email_dict['to']:
+                    officer_notification = Notification(
+                        type=Notification.TYPE_OFFICER,
+                        win=win,
+                        recipient=officer_email,
+                    )
+                    officer_notification.save()
--- wins/management/commands/win_info.py
+++ wins/management/commands/win_info.py
@@ -7,18 +7,19 @@ from ...models import Win
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument("--id", type=str)
+        parser.add_argument("--ids", type=str)
 
     def handle(self, *args, **options):
 
-        win_id = options['id'].replace('-', '')
-        try:
-            win = Win.objects.get(id=win_id)
-        except Win.DoesNotExist:
-            print('no such win')
-        else:
-            print(
-                'win.id', win.id,
-                'win.customer_email_address', win.customer_email_address,
-                'win.customer_name', win.customer_name
-            )
+        win_ids = [wid.replace('-', '') for wid in options['ids'].split(',')]
+        for win_id in win_ids:
+            try:
+                win = Win.objects.get(id=win_id)
+            except Win.DoesNotExist:
+                print('no such win')
+            else:
+                print(
+                    'win.id', win.id,
+                    'win.customer_email_address', win.customer_email_address,
+                    'win.customer_name', win.customer_name
+                )
