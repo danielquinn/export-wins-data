af338986f666284dbdb63db41834d7c5832ccd36
bmcorser <benmarshallcorser@gmail.com>
2016-07-18 09:58:20 +0100

    Sketched out flattened CSV export

--- wins/views/__init__.py
+++ wins/views/__init__.py
@@ -1,9 +1,8 @@
-
+import collections
 import csv
 import io
 import zipfile
 
-from django.db import connection
 from django.http import HttpResponse
 
 from rest_framework import authentication, permissions
@@ -15,15 +14,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from . import notifications
-from .filters import CustomerResponseFilterSet
-from .models import Win, Breakdown, Advisor, CustomerResponse, Notification
-from .serializers import (
+from .. import notifications
+from ..filters import CustomerResponseFilterSet
+from ..models import Win, Breakdown, Advisor, CustomerResponse, Notification
+from ..serializers import (
     WinSerializer, LimitedWinSerializer, BreakdownSerializer,
     AdvisorSerializer, CustomerResponseSerializer, NotificationSerializer
 )
 from alice.authenticators import AlicePermission
 from alice.views import AliceMixin
+
+from .flat_csv import CSVView  # just so CSVView appears on views module
 
 
 class StandardPagination(PageNumberPagination):
@@ -123,28 +124,3 @@ class AdvisorViewSet(AliceMixin, ModelViewSet):
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     ordering_fields = ("pk",)
     http_method_names = ("get", "post")
-
-
-class CSVView(APIView):
-    permission_classes = (permissions.IsAdminUser,)
-
-    def make_csv(self, table):
-        """ Get CSV of table """
-
-        stringio = io.StringIO()
-        cursor = connection.cursor()
-        cursor.execute("select * from wins_{};".format(table))
-        csv_writer = csv.writer(stringio)
-        header = [i[0] for i in cursor.description]
-        csv_writer.writerow(header)
-        csv_writer.writerows(cursor)
-        return stringio.getvalue()
-
-    def get(self, request, format=None):
-        bytesio = io.BytesIO()
-        zf = zipfile.ZipFile(bytesio, 'w')
-        for table in ['win', 'customerresponse', 'notification', 'advisor']:
-            csv_str = self.make_csv(table)
-            zf.writestr(table + 's.csv', csv_str)
-        zf.close()
-        return HttpResponse(bytesio.getvalue())
---
+++ wins/views/flat_csv.py
@@ -0,0 +1,82 @@
+import collections
+import csv
+import io
+import zipfile
+
+from django.http import HttpResponse
+
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..models import Win, Breakdown, Advisor, CustomerResponse, Notification
+from ..serializers import (
+    WinSerializer, LimitedWinSerializer, BreakdownSerializer,
+    AdvisorSerializer, CustomerResponseSerializer, NotificationSerializer
+)
+
+
+def flatten_user(attr):
+    return "{0} <{1}>".format(attr.name, attr.email)
+
+
+def flatten_advisors(attr):
+    return ', '.join(map(str, attr.all()))
+
+
+def flatten_breakdowns(attr):
+    return ', '.join(map(str, attr.all()))
+
+
+def flatten_notifications(attr):
+    return any([x.type == 'c' for x in attr.all()])
+
+
+def flatten_confirmation(attr):
+    return True  # isn’t called unless the field is present
+
+
+class CSVView(APIView):
+    permission_classes = (permissions.IsAdminUser,)
+
+    def get_serializer(self):
+        return WinSerializer
+
+    def metadata_class(self):
+        serializer_metadata = metadata_class.get_serializer_info(serializer)
+        return Response(serializer_metadata)
+
+    def make_csv(self):
+        """ Get CSV of table """
+        fkeys = [
+            ('user', flatten_user),
+            ('advisors', flatten_advisors),
+            ('breakdowns', flatten_breakdowns),
+            ('notifications', flatten_notifications),
+            ('confirmation', flatten_confirmation),
+        ]
+        stringio = io.StringIO()
+        wins = Win.objects.all()
+        win_dicts = []
+        for win in wins:
+            win_dict = collections.OrderedDict()
+            for field in WinSerializer().fields:
+                win_dict[field] = getattr(win, field)
+            for name, func in fkeys:
+                try:
+                    win_dict.update([(name, func(getattr(win, name)))])
+                except (CustomerResponse.DoesNotExist,) as exc:
+                    win_dict.update([('confirmation', False)])
+            win_dicts.append(win_dict)
+        csv_writer = csv.DictWriter(stringio, win_dicts[0].keys())
+        csv_writer.writeheader()
+        csv_writer.writerows(win_dicts)
+        return stringio.getvalue()
+
+    def get(self, request, format=None):
+        bytesio = io.BytesIO()
+        zf = zipfile.ZipFile(bytesio, 'w')
+        csv_str = self.make_csv()
+        zf.writestr('wins.csv', csv_str)
+        zf.close()
+        return HttpResponse(bytesio.getvalue())
