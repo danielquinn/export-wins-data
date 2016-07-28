557dacec4e1ac4d65cd533afe6365f865510827e
Adam Malinowski <adam.malinowski@digital.bis.gov.uk>
2016-07-28 16:13:10 +0100

    Minor factoring of customer email generation

--- wins/management/commands/generate_customer_emailses.py
+++ wins/management/commands/generate_customer_emailses.py
@@ -24,6 +24,42 @@ class Command(BaseCommand):
         assert not missing_win_ids, "missing win ids: %s" % missing_win_ids
         return wins
 
+    def send_win_customer_email(self, win):
+
+        # should not be hardcoded
+        url = "https://www.exportwins.ukti.gov.uk/wins/review/" + str(win.pk)
+        customer_email_dict = generate_customer_email(url, win)
+        send_mail(
+            customer_email_dict['subject'],
+            customer_email_dict['body'],
+            settings.FEEDBACK_ADDRESS,
+            customer_email_dict['to'],
+        )
+        customer_notification = Notification(
+            type=Notification.TYPE_CUSTOMER,
+            win=win,
+            recipient=customer_email_dict['to'][0],
+        )
+        customer_notification.save()
+
+    def send_officer_email(self, win):
+
+        officer_email_dict = generate_officer_email(win)
+        send_mail(
+            officer_email_dict['subject'],
+            officer_email_dict['body'],
+            settings.SENDING_ADDRESS,
+            officer_email_dict['to'],
+        )
+        for officer_email in officer_email_dict['to']:
+            officer_notification = Notification(
+                type=Notification.TYPE_OFFICER,
+                win=win,
+                recipient=officer_email,
+            )
+            officer_notification.save()
+
+
     def handle(self, *args, **options):
         """ Quick hack to generate txt of customer emails and corresponding
         officer emails.
@@ -66,35 +102,6 @@ class Command(BaseCommand):
             assert False, 'no valid flag given'
 
         for win in wins:
-
-            # should not be hardcoded
-            url = "https://www.exportwins.ukti.gov.uk/wins/review/" + str(win.pk)
-            customer_email_dict = generate_customer_email(url, win)
-            send_mail(
-                customer_email_dict['subject'],
-                customer_email_dict['body'],
-                settings.FEEDBACK_ADDRESS,
-                customer_email_dict['to'],
-            )
-            customer_notification = Notification(
-                type=Notification.TYPE_CUSTOMER,
-                win=win,
-                recipient=customer_email_dict['to'][0],
-            )
-            customer_notification.save()
-
+            self.send_win_customer_email(win)
             if not change:
-                officer_email_dict = generate_officer_email(win)
-                send_mail(
-                    officer_email_dict['subject'],
-                    officer_email_dict['body'],
-                    settings.SENDING_ADDRESS,
-                    officer_email_dict['to'],
-                )
-                for officer_email in officer_email_dict['to']:
-                    officer_notification = Notification(
-                        type=Notification.TYPE_OFFICER,
-                        win=win,
-                        recipient=officer_email,
-                    )
-                    officer_notification.save()
+                self.send_officer_email(win)
